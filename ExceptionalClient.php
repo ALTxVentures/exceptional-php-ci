3f5562876e3c2b6f043e0c3b94c27ca92a6c9789
Jan Lehnardt <jan@dahlia.(none)>
2008-09-12 15:34:57 +0200

    cleanup, add docs

--- ExceptionalClient.php
+++ ExceptionalClient.php
@@ -8,92 +8,119 @@ class ExceptionalClient
 {
     /**
      * Installs the ExceptinoalClient class as the default exception handler
-     *
+	 *
+     * @param string api_key from getexceptional.com
      **/
-    function __construct($api_key)
+    function __construct($api_key, $debugging = false)
     {
         $this->url = "/errors/?api_key={$api_key}&protocol_version=2";
         $this->host = "getexceptional.com";
         $this->port = 80;
-
+		$this->debugging = $debugging;
+		
         // set exception handler & keep old exception handler around
-        $this->previous_exception_handler = set_exception_handler(array($this, "handle_exception"));
+        $this->previous_exception_handler = 
+			set_exception_handler(array($this, "handle_exception"));
     }
     
+	/**
+	 * Exception handle class. Pushes the current exception onto the exception
+	 * stack and calls the previous handler, if it exists. Ensures seamless
+	 * integration.
+	 *
+	 * @param Exception exception object, gets passed in by PHP
+	 **/
     function handle_exception($exception)
     {
-        
         $this->exceptions[] = new ExceptionData($exception);
+
+		// If there's a previous exception handler, we call that as well
         if($this->previous_exception_handler) {
-            $this->previous_exception_handler();
+            $this->previous_exception_handler($exception);
         }
     }
     
+	/**
+	 * Destructor! Sends all collected exceptions to Exceptional
+	 *
+	 **/
     function __destruct()
     {
-        echo "sending exceptions:";
         // send stack of exceptions to getexceptional
         foreach($this->exceptions AS $exception) {
             $this->send_exception($exception);
         }
     }
     
+	/**
+	 * Does the actual sending of an exception
+	 *
+	 * @param Exception exception object, gets passed in by PHP
+	 **/
     function send_exception($exception)
     {
         $body = $exception->toXML();
-        
         $this->post($this->url, $body);
     }
     
+	/**
+	 * Sends a POST request
+	 *
+	 * @param String url to post to on `$this->server`
+	 * @param String post data to send as body
+	 **/
     function post($url, $post_data)
     {
         $s = fsockopen($this->host, $this->port, $errno, $errstr); 
 
-        if(!$s) { 
-            echo "$errno: $errstr\n";
+        if(!$s || empty($post_data)) { 
             return false;
         }
 
         $request = "POST $url HTTP/1.1\r\nHost: $this->host\r\n";
+        $request .= "Accept: */*\r\n";
+        $request .= "User-Agent: exception-php-client 0.1\r\n";
+        $request .= "Content-Type: text/xml\r\n";
+        $request .= "Connection: close\r\n";
+        $request .= "Content-Length: ".strlen($post_data)."\r\n\r\n";
 
-        if($post_data) {
-            $request .= "Accept: */*\r\n";
-            $request .= "User-Agent: exception-php-client 0.1\r\n";
-            $request .= "Content-Type: text/xml\r\n";
-            $request .= "Connection: close\r\n";
-            $request .= "Content-Length: ".strlen($post_data)."\r\n\r\n";
-            $request .= "$post_data\r\n";
-        } else {
-            $request .= "\r\n";
-        }
+        $request .= "$post_data\r\n";
 
-        var_dump($request);
-flush();
         fwrite($s, $request);
-        $response = "";
-        while(!feof($s)) {
-            $response .= fgets($s);
-        }
 
-        var_dump($response);
-        list($this->headers, $this->body) = explode("\r\n\r\n", $response);
-        // return $this->body;
+		if(!$this->debugging) {
+			return; // do not wait for response, we don't care
+		} else {
+			// for debugging
+	        $response = "";
+	        while(!feof($s)) {
+	            $response .= fgets($s);
+	        }
+			var_dump($response);
+		}
     }
 }
 
+/**
+ * Wrapper class for data sent to exceptional API
+ *
+ * @package ExceptionalClient
+ **/
 class ExceptionData
 {
     function __construct($exception)
     {
         $this->exception = $exception;
-        $this->user_ip = $_SERVER["REMOTE_ADDR"];
-        $this->host_ip = $_SERVER["SERVER_ADDR"];
-        $this->request_method = $_SERVER["REQUEST_METHOD"];
-        $this->request_uri = $_SERVER["REQUEST_URI"];
     }
     
     function toXML()
     {
+        $user_ip = $_SERVER["REMOTE_ADDR"];
+        $host_ip = $_SERVER["SERVER_ADDR"];
+        $request_uri = $_SERVER["REQUEST_URI"];
+		$document_root = $_SERVER["DOCUMENT_ROOT"];
+        $request_method = $_SERVER["REQUEST_METHOD"];
+
         $now = date("D M j H:i:s O Y");
         $env = $this->envToXML();
         $session = $this->sessionToXML();
@@ -119,8 +146,8 @@ $env
   <session>
 $session
   </session>
-  <rails_root>/</rails_root>
-  <url>$this->request_uri</url>
+  <rails_root>$document_root</rails_root>
+  <url>$request_uri</url>
   <parameters>
 $request_parameters
   </parameters>
@@ -159,26 +186,4 @@ $request_parameters
         
         return implode("\n", $return_value);
     }
-}
-/*
-POST to http://getexceptional.com/errors/?api_key=1234yuio123yiuo&protocol_version=2
-
-<?xml version="1.0" encoding="UTF-8"?><error><agent_id>cc25f30e09d5d2e14cbdc7b0e1da30ba0896a58c</agent_id>
-<controller_name>Foo</controller_name>
-<action_name>bar</action_name>
-<error_class>DodgyException</error_class>
-<message>this is awesome</message>
-<backtrace>the craziness</backtrace>
-<occurred_at>Thu Sep 11 16:05:53 -0400 2008</occurred_at>
-<rails_root>/var/www/woo/woo/woo</rails_root>
-<url>http://www.tryme.com</url>
-<environment>
- <server_name>lovely</server_name>
- <foo>nice</foo>
-</environment>
-<session>
- <foo>bar</foo>
-</session>
-<parameters>POST DATA</parameters>
-</error>"
-*/
+} /* class ExceptionData */
