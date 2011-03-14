633fdba6a47a25211168abc94071b5db5010e59c
Andrew Kane <andrew@getformidable.com>
2011-03-13 23:44:01 -0700

    Added PHP_AUTH_PW to vars removed from

--- exceptional/environment.php
+++ exceptional/environment.php
@@ -2,25 +2,29 @@
 
 class ExceptionalEnvironment
 {
-    
+
     private static $environment;
-    
+
     static function to_array()
-    {       
+    {
         if (!self::$environment) {
             $env = $_SERVER;
-            $vars = array("PHPSELF", "SCRIPT_NAME", "SCRIPT_FILENAME", "PATH_TRANSLATED", "DOCUMENT_ROOT", "PHP_SELF", "argv", "argc", "REQUEST_TIME");
+
+            // remove the following $_SERVER variables
+            $vars = array("PHPSELF", "SCRIPT_NAME", "SCRIPT_FILENAME", "PATH_TRANSLATED", "DOCUMENT_ROOT", "PHP_SELF", "argv", "argc", "REQUEST_TIME", "PHP_AUTH_PW");
             foreach ($vars as $var) {
-                if (!isset($env[$var])) continue;
-                unset($env[$var]);
+                if (isset($env[$var])) {
+                    unset($env[$var]);
+                }
             }
-            
+
+            // remove variables that begin with HTTP_
             foreach ($env as $k => $v) {
                 if (substr($k, 0, 5) == "HTTP_") {
                   unset($env[$k]);
                 }
             }
-        
+
             self::$environment =  array(
                 "client" => array(
                   "name" => Exceptional::$client_name,
@@ -43,7 +47,7 @@ class ExceptionalEnvironment
 
         return self::$environment;
     }
-    
+
     static function get_username() {
         $vars = array("LOGNAME", "USER", "USERNAME", "APACHE_RUN_USER");
         foreach ($vars as $var) {
@@ -53,12 +57,12 @@ class ExceptionalEnvironment
         }
         return "UNKNOWN";
     }
-    
+
     static function get_root_dir() {
         if (isset($_SERVER["PWD"])) {
             return $_SERVER["PWD"];
         }
         return @$_SERVER["DOCUMENT_ROOT"];
     }
-    
+
 }
