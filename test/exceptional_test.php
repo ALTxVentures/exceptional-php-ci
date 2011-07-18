58e8d7e71fa784ac53a09fbc3ec290081d78f71b
Andrew Kane <andrew@getformidable.com>
2011-07-17 17:05:59 -0700

    Updated tests

--- test/exceptional_test.php
+++ test/exceptional_test.php
@@ -17,22 +17,24 @@ class ExceptionalTest extends PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        Exceptional::setup("");
         $_SERVER["HTTP_HOST"] = "localhost";
     }
 
-    function testParameters()
+    function testGetParameters()
     {
         $_GET["a"] = "GET works";
+
+        $this->createExceptionData();
+
+        $this->assertEquals($this->request["parameters"]["a"], "GET works");
+    }
+
+    function testPostParameters() {
         $_POST["b"] = "POST works";
 
-        $notice = new PhpNotice("Test", 0, "", 0);
-        $data = new ExceptionalData($notice);
+        $this->createExceptionData();
 
-        $params = $data->data["request"]["parameters"];
-
-        $this->assertEquals($params["a"], "GET works");
-        $this->assertEquals($params["b"], "POST works");
+        $this->assertEquals($this->request["parameters"]["b"], "POST works");
     }
 
     function testControllerAndAction()
@@ -40,13 +42,26 @@ class ExceptionalTest extends PHPUnit_Framework_TestCase
         Exceptional::$controller = "home";
         Exceptional::$action = "index";
 
+        $this->createExceptionData();
+
+        $this->assertEquals($this->request["controller"], "home");
+        $this->assertEquals($this->request["action"], "index");
+    }
+
+    function testSessionFilter() {
+        $session_name = ini_get("session.name");
+        $session_id = rand();
+        $_SERVER["HTTP_Cookie"] = "$session_name=$session_id";
+
+        $this->createExceptionData();
+
+        $this->assertEquals($this->request["headers"]["Cookie"], "$session_name=[FILTERED]");
+    }
+
+    function createExceptionData() {
         $notice = new PhpNotice("Test", 0, "", 0);
-        $data = new ExceptionalData($notice);
-
-        $request = $data->data["request"];
-
-        $this->assertEquals($request["controller"], "home");
-        $this->assertEquals($request["action"], "index");
+        $this->data = new ExceptionalData($notice);
+        $this->request = $this->data->data["request"];
     }
 
 }
